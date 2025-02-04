331dc09c54df64533c37162dcb0deac11f67a7b5
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2025-02-04 15:23:09 +0100

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Lorenzo Menino on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitesseTests/VitesseTests.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseTests.swift
+//  VitesseTests
+//
+//  Created by Lorenzo Menino on 04/02/2025.
+//
+
+import Testing
+@testable import Vitesse
+
+struct VitesseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
